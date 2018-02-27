ff62f7cebdde3e92c296e1ebe0cdeb95517b926c
WillMueller69 <31865845+WillMueller69@users.noreply.github.com>
2018-02-27 13:34:06 -0800

    Add files via upload
    Latest and Greatest

--- quadscripts.js
+++ quadscripts.js
@@ -139,10 +139,10 @@
 
 
 //Javascript for drawing and calculating quadratics 2017
-var a, b, c, context, w, h, k = 10;
+var a, b, c, ctx, w, h, k = 10;
 var zoom = 18;
-var w = canvas.width;
-var h = canvas.height;
+// var w = canvas.width;
+// var h = canvas.height;
 
 
 function init() {
@@ -150,6 +150,10 @@ function init() {
     ctx = canvas.getContext("2d");
     w = canvas.width = 600;
     h = canvas.height = 400;
+    canvasOffset = canvas.getBoundingClientRect();
+    offsetX = Math.round(canvasOffset.left),
+        offsetY = Math.round(canvasOffset.top);
+    canvas.addEventListener("mousemove", doMouseMove, false);
     console.log('canvas is loaded into context', w);
 } // close init
 
@@ -158,6 +162,10 @@ function QF() {
     a = $("#linA").val();
     b = $("#linB").val();
     c = $("#constant").val();
+    d = b**2-4*a*c;
+    if(d<0){
+        $("#solution1").text("No x-intercepts")
+    }
     var discriminant = (b ** 2) - (4 * a * c);
     var other_part = -b;
     var bottom_part = 2 * a;
@@ -166,24 +174,26 @@ function QF() {
         var quadForm_answer2 = "Your Answer Is Undefined";
         $("#quadformanswer").text(quadForm_answer1);
         $("#quadformanswer2").text(quadForm_answer2);
-    } else {
+    } 
         quadForm_answer1 = (other_part + Math.sqrt(discriminant)) / bottom_part;
         quadForm_answer2 = (other_part - Math.sqrt(discriminant)) / bottom_part;
-        console.log(quadForm_answer1);
-        console.log(quadForm_answer2);
+        //console.log(quadForm_answer1);
+        //console.log(quadForm_answer2);
         // var answer = (-b+((b**2)-(4*a*c))/(2*a);
         // console.log(answer)
         console.log(a, b, c);
         $("#quadformanswer").text(quadForm_answer1);
         $("#quadformanswer2").text(quadForm_answer2);
         ctx.fillStyle = "red"
+        ctx.beginPath();
         ctx.arc((w / 2) + (quadForm_answer1 * zoom), (h / 2), 6, 0, 2 * Math.PI);
         ctx.fill();
+        ctx.beginPath();
         ctx.arc((w / 2) + (quadForm_answer2 * zoom), (h / 2), 6, 0, 2 * Math.PI);
         ctx.fill();
 
-        
-    }
+
+    
 
     results();
     graphpaper();
@@ -191,8 +201,7 @@ function QF() {
     horizLineGrapher();
     vertLineGrapher2();
     horizLineGrapher2();
-} // close QF
-
+}
 function results() {
     // finding vertext and displaying symline and yint results
     vX = -(b * 1) / (2 * a);
@@ -202,11 +211,12 @@ function results() {
 
     $("#vertex").text("Vertex is at (" + vX + "," + vY + ")");
     $("#yInt").text("The Y Intercept is at (0," + c + ")");
-    ctx.fillStyle = "red";
+    //ctx.fillStyle = "red";
     ctx.beginPath();
     ctx.arc((w / 2) + (vX * zoom), (h / 2) - (vY * zoom), 6, 0, 2 * Math.PI);
     ctx.fill();
-    ctx.arc((w/2),(h/2) - (c*zoom), 6, 0, 2 * Math.PI);
+    ctx.beginPath();
+    ctx.arc((w / 2), (h / 2) - (c * zoom), 6, 0, 2 * Math.PI);
     ctx.fill();
     ctx.stroke();
     $("#correspondingPoint").text("Corresponding Point is at(" + 2 * vX + "," + c + ")")
@@ -215,7 +225,7 @@ function results() {
 
 function graphpaper() {
     // the x and y axis drawn
-    ctx.strokeStyle = "#DCDADA"
+    ctx.strokeStyle = "rgba(225, 222, 222, 0.51)"
     ctx.lineWidth = 5;
     ctx.beginPath();
     ctx.moveTo(0, h / 2);
@@ -230,7 +240,7 @@ function vertLineGrapher() {
     ctx.stokeStyle = "rgba(0,255,0,.5)";
     var vlX = w / 2;
     var vlY = 0;
-    ctx.fillStyle = "#DCDADA";
+    ctx.fillStyle = "rgba(225, 222, 222, 0.51)";
     ctx.fillRect(295, 195, 10, 10);
     while (vlX < w) {
         vlX = vlX + zoom;
@@ -241,7 +251,7 @@ function vertLineGrapher() {
 function vertLineGrapher2() {
     var vlX2 = w / 2;
     var vlY2 = 0;
-    ctx.fillStyle = "#DCDADA";
+    ctx.fillStyle = "rgba(225, 222, 222, 0.51)";
     ctx.fillRect(295, 195, 10, 10);
     while (vlX2 > 0) {
         vlX2 = vlX2 - zoom;
@@ -253,7 +263,7 @@ function vertLineGrapher2() {
 function horizLineGrapher() {
     var hlX = 0;
     var hlY = h / 2;
-    ctx.fillStyle = "#DCDADA";
+    ctx.fillStyle = "rgba(225, 222, 222, 0.51)";
     while (hlY < 400) {
         hlY = hlY + zoom;
         ctx.fillRect(hlX, hlY, w, 2);
@@ -263,7 +273,7 @@ function horizLineGrapher() {
 function horizLineGrapher2() {
     var hlX2 = 0;
     var hlY2 = h / 2;
-    ctx.fillStyle = "#DCDADA";
+    ctx.fillStyle = "rgba(225, 222, 222, 0.51)";
     while (hlY2 > 0) {
         hlY2 = hlY2 - zoom;
         ctx.fillRect(hlX2, hlY2, w, 2);
@@ -281,13 +291,14 @@ function zoomOut() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
+
 function graphQuad() {
     for (var i = 0; i < w; i++) {
         x = (w / 2 - i) / zoom;
         y = c * 1 + b * x + a * Math.pow(x, 2);
         nx = (w / 2 - (i + 1)) / zoom;
         ny = c * 1 + b * nx + a * Math.pow(nx, 2);
-        console.log(x, y, nx, ny)
+        //console.log(x, y, nx, ny)
         ctx.beginPath();
         ctx.lineWidth = 2;
         ctx.strokeStyle = "Red";
@@ -295,4 +306,36 @@ function graphQuad() {
         ctx.lineTo((w / 2) + (nx * zoom), (h / 2) - (ny * zoom));
         ctx.stroke();
     }
+}
+
+function doMouseMove(event) {
+    redrawCanvas();
+    // always know where ther mouse is located
+    mouseX = event.clientX - offsetX;
+    mouseY = event.clientY - offsetY;
+    pointX = (mouseX - w / 2) / zoom;
+    pointY = a * Math.pow(pointX, 2) + b * pointX + c * 1;
+    pointX = pointX.toFixed(2);
+    pointY = pointY.toFixed(2);
+   // console.log(mouseX, mouseY, pointX, pointY, offsetY, offsetX);
+    ctx.beginPath();
+    ctx.arc(mouseX, h / 2 - pointY * zoom, 5, 0, 2 * Math.PI);
+    ctx.fill();
+    $("#pointOnGraph").text("Point on the curve: (" + pointX + "," + pointY + ")");
+
+} // end doMouseMove
+
+function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+function redrawCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    graphpaper();
+    vertLineGrapher();
+    horizLineGrapher();
+    vertLineGrapher2();
+    horizLineGrapher2();
+    graphQuad();
+    results();
 }
